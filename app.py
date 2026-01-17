fd952e78a5454ddec4600da78657559a7e449843
Zadie Monroe <zadieemonroe@gmail.com>
2026-01-17 09:58:24 +0200

    added list

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
- from flask import Flask
-app  = Flask (__name__)
 
-@app.route('/')
-def home():
-    return "Homepage<h1>Welcome to the Homepage</h1>"
-if __name__=='_main_':
-    app.run(debug=True)
+notes=[]
+def add_note(note):
+    notes.append(note)
+def get_notes():
+    return notes
+print("Note-taking app is running.")
+def rename_note(note):
+    if note in notes:
+        new_name = input("Enter the new name for the note: ")
+        index = notes.index(note)
+        notes[index] = new_name
+        print(f"Note renamed to: {new_name}")
+    else:
+        print("Note not found.")
+def delete_note(note):
+            if note in notes:
+                notes.remove(note)
+                print(f"Note '{note}' deleted.")
+            else:
+                print("Note not found.")
+
